ccffd769bdfacec20338b1ddaaf9c84ef478d866
Afonso Alcantara <alcantara.afonso@gmail.com>
2023-01-11 19:06:26 -0300

    Spring Boot com JPA - formas de se fazer consultas

--- src/main/java/br/com/beganinha/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/br/com/beganinha/exerciciossb/controllers/ProdutoController.java
@@ -1,18 +1,26 @@
 package br.com.beganinha.exerciciossb.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.beganinha.exerciciossb.model.entities.Produto;
 import br.com.beganinha.exerciciossb.model.repositories.ProdutoRepository;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
-	
+
 	/*
 	 * Essa anotação faz a injeção de dependência e o Inversion of control (IoC)
 	 */
@@ -20,17 +28,87 @@ public class ProdutoController {
 	private ProdutoRepository produtoRepository;
 
 	/**
-	 * O Spring consegue instanciar um objeto produto a partir do JSON recebido
-	 * Isso só funcionará se as propriedades do JSON forem identicas aos atributos da entidade
+	 * O Spring consegue instanciar um objeto produto a partir do JSON recebido Isso
+	 * só funcionará se as propriedades do JSON forem identicas aos atributos da
+	 * entidade
+	 * 
 	 * @param produto
 	 * @return
 	 */
-	@PostMapping
-	public @ResponseBody Produto novoProduto(Produto produto) {
-		
+
+	/*
+	 * Quando um método JAVA é capaz de atender a dois métodos HTTP, o recomendado é
+	 * que se use a anotação @RequestMapping e seja usado o parâmetro method
+	 */
+//	@PostMapping
+	@RequestMapping(method = { RequestMethod.POST, RequestMethod.PUT })
+	public @ResponseBody Produto salvarProduto(@Valid Produto produto) {
+
 		produtoRepository.save(produto);
-		
+
 		return produto;
 	}
+
+	@GetMapping
+	public @ResponseBody Iterable<Produto> getProdutos() {
+		Iterable<Produto> produtos = produtoRepository.findAll();
+
+		return produtos;
+	}
+
+	/**
+	 * Consulta paginada
+	 * 
+	 * @return
+	 */
+	@GetMapping(path = "/pagina/{pageNumber}/{quantity}")
+	public @ResponseBody Iterable<Produto> getProdutosByPage(@PathVariable int pageNumber, @PathVariable int quantity) {
+		Pageable page = PageRequest.of(pageNumber, quantity);
+		Iterable<Produto> produtos = produtoRepository.findAll(page);
+
+		return produtos;
+	}
+
+	@GetMapping(path = "/{id}")
+	public @ResponseBody Optional<Produto> getProdutoById(@PathVariable int id) {
+
+		return produtoRepository.findById(id);
+	}
+
+	/**
+	 * o deleteById faz um hard delete+
+	 * 
+	 * @param id
+	 */
+	@DeleteMapping(path = "/{id}")
+	public void deleteProdutoById(@PathVariable int id) {
+
+		produtoRepository.deleteById(id);
+	}
 	
+	@GetMapping(path = "/nome/{partNome}")
+	public @ResponseBody Iterable<Produto> getProdutoByName(@PathVariable String partNome) {
+
+		Iterable<Produto> produtos = produtoRepository.findByNomeContaining(partNome);
+		
+		return produtos;
+	}
+
+	@GetMapping(path = "/query/{partNome}")
+	public @ResponseBody Iterable<Produto> getProdutoByNameQuery(@PathVariable String partNome) {
+
+		Iterable<Produto> produtos = produtoRepository.searchByNameLike(partNome);
+		
+		return produtos;
+	}
+
+
+//	@PutMapping
+//	public @ResponseBody Produto changeProduto(@Valid Produto produto) {
+//		
+//		produtoRepository.save(produto);
+//		
+//		return produto;
+//				
+//	}
 }
--- src/main/java/br/com/beganinha/exerciciossb/model/repositories/ProdutoRepository.java
+++ src/main/java/br/com/beganinha/exerciciossb/model/repositories/ProdutoRepository.java
@@ -1,10 +1,50 @@
 package br.com.beganinha.exerciciossb.model.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import br.com.beganinha.exerciciossb.model.entities.Produto;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
-
+/**
+ * A interface PagingAndSortingRepository provê uma forma simples de fazer consulta paginada
+ *
+ */
+public interface ProdutoRepository extends 
+			CrudRepository<Produto, Integer>, PagingAndSortingRepository<Produto, Integer> {
 	
+	/**
+	 * O SpringBoot implementará esse método
+	 * Para que essa implmentação funcione, é necessário que o nome do método obedeça a convenção do Spring, exemplo:
+	 * Para realizar uma busca, o método precisa começar com "find" e como se deseja que faça uma like precisa-se que
+	 * o find seja seguindo de By<O nome do atributo definido na entidade que se que busca>Containing
+	 * OBS: Deve-se usar o nome exato do atributo definido na entidade 
+	 * @param name
+	 * @return
+	 */
+	public Iterable<Produto> findByNomeContaining(String name);
+	
+	/**
+	 * Seguindo a convenção, o método fará um lile ignorando CAPS LOCK
+	 * @param name
+	 * @return
+	 */
+	public Iterable<Produto> findByNomeContainingIgnoreCase(String name);
+	
+	public Iterable<Produto> findByNomeIsContaining(String name);
+	
+	public Iterable<Produto> findByNomeContains(String name);
+	
+	public Iterable<Produto> findByNomeStartsWith(String name);
+	
+	public Iterable<Produto> findByNomeEndsWith(String name);
+	
+	public Iterable<Produto> findByNomeNotContaining(String name);
+	
+	@Query("SELECT p FROM Produto p WHERE p.nome LIKE %:nome%")
+	public Iterable<Produto> searchByNameLike(@Param("nome") String name);
+	
+	
+
 }
